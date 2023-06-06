32d5d51820268141959eba5a8a9e93360d09fd01
jessiemullins <117420244+jessiemullins@users.noreply.github.com>
2023-06-06 14:13:44 -0400

    added about and home files

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from "../components/Navigation"
+import Footer from '../components/Footer'
+import AboutContent from '../components/AboutContent'
+
+const About = () => {
+    return (
+    <div>
+        <Navigation />
+        <AboutContent />
+        <Footer />
+    </div>
+    )
+};
+export default About;
